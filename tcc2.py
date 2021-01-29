2bf1961b709bea45497d12fdd165eb639c69f868
CCSAN313 <77705117+CCSAN313@users.noreply.github.com>
2021-01-28 21:12:10 -0300

    Add files via upload

--- tcc2.py
+++ tcc2.py
@@ -8,7 +8,6 @@ banco = mysql.connector.connect(
     user="root",
     passwd="",
     database="cadastro_tcc",
-    port=3306
 )
 
 
@@ -26,24 +25,32 @@ def funcao_principal():
     linha4 = apkk_tcc.lineEdit_4.text()
     print("NOME:", linha1)
     print("TELEFONE:", linha2)
-    print("USUÁRIO:", linha3)
+    print("USUARIO:", linha3)
     print("SENHA:", linha4)
 
     cursor = banco.cursor()
-    comando_SQL = "INSERT INTO cadastro_tcc (codigo,nome,telefone,usuário,senha) VALUES(%s,%s,%s,%s,%s)"
-    dados = (str(linha), str(linha2), str(linha3), str(linha4))
+
+    comando_SQL = "INSERT INTO 'cadastro_tcc'('nome','telefone','usuário','senha') VALUES ('%s','%s','%s','%s')"
+    dados = (str(linha1), str(linha2), str(linha3), str(linha4))
     cursor.execute(comando_SQL, dados)
     banco.comit()
 
 
 def chama_segunda_tela():
-    formulario = ui.loadUi('apkk_tcc.ui')
+    formulario.show()
+
+
+def envio_formulario():
+    comando_SQL.send()
 
 
 app = QtWidgets.QApplication([])
-primeira_tela = uic.loadUi('login.ui')
-formulario = uic.loadUi('apkk_tcc.ui')
-primeira_tela.pushButton_2.clicked.connect(funcao_principal)
+primeira_tela = uic.loadUi("login.ui")
+formulario = uic.loadUi("apkk_tcc.ui")
+primeira_tela.pushButton.clicked.connect(funcao_principal)
+primeira_tela.pushButton_2.clicked.connect(chama_segunda_tela)
+formulario.pushButton.clicked.connect(envio_formulario)
+
 
 primeira_tela.show()
 app.exec()
